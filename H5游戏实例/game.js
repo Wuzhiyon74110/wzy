4298aa5b7d661d9ecead3630efc0042e06b9fd32
Wuzhiyon74110 <591525527@qq.com>
2019-11-22 16:21:31 +0800

    game2

--- H5游戏实例/game.js
+++ H5游戏实例/game.js
@@ -5,8 +5,8 @@ var gqs = 0;//关卡数
 var tj = 5;//通关条件
 var hp1 = 100;//怪物1hp
 var hp2 = 100;//怪物2hp
-canvas.width = 544;//画布宽度定义
-canvas.height = 416;//画布高度定义
+canvas.width = 512;//画布宽度定义
+canvas.height = 480;//画布高度定义
 document.body.appendChild(canvas);
 
 // 背景图片属性
@@ -23,14 +23,14 @@ var heroImage = new Image();
 heroImage.onload = function () {
 	heroReady = true;
 };
-heroImage.src = "images/dijia.png";
+heroImage.src = "images/1.png";
 
 var BulletReady = false;
 var BulletImage = new Image();
 BulletImage.onload = function () {
 	BulletReady = true;
 };
-BulletReady.src = "images/gemola.png";
+BulletReady.src = "images/2.png";
 
 // 怪物1属性
 var monsterReady = false;
@@ -41,10 +41,10 @@ monsterImage.onload = function () {
 var m1 = Math.round(Math.random())
 // alert(m1);
 if (m1 == 0) {
-	monsterImage.src = "images/gemola.png";
+	monsterImage.src = "images/2.png";
 
 } else {
-	monsterImage.src = "images/geerzan.png";
+	monsterImage.src = "images/3.png";
 
 }
 //怪物2属性
@@ -56,10 +56,10 @@ monsterImage2.onload = function () {
 var m1 = Math.round(Math.random())
 // alert(m1);
 if (m1 == 0) {
-	monsterImage2.src = "images/gemola.png";
+	monsterImage2.src = "images/2.png";
 
 } else {
-	monsterImage2.src = "images/geerzan.png";
+	monsterImage2.src = "images/3.png";
 
 }
 
@@ -140,10 +140,10 @@ var update = function (modifier) {
 			hp1=100;
 			var m1 = Math.round(Math.random())
 			if (m1 == 0) {
-				monsterImage.src = "images/gemola.png";
+				monsterImage.src = "images/2.png";
 
 			} else {
-				monsterImage.src = "images/geerzan.png";
+				monsterImage.src = "images/3.png";
 
 			}
 
@@ -168,10 +168,10 @@ var update = function (modifier) {
 		hp2=100;
 		var m1 = Math.round(Math.random())
 		if (m1 == 0) {
-			monsterImage2.src = "images/gemola.png";
+			monsterImage2.src = "images/2.png";
 
 		} else {
-			monsterImage2.src = "images/geerzan.png";
+			monsterImage2.src = "images/3.png";
 
 		}
 
